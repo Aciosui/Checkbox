0e6206a81196f084b242c2be7e03e62399b406f1
aciosui <mrana.swe@gmail.com>
2024-04-01 13:16:38 -0400

    Initial Commit

---
+++ Checkbox/CheckboxApp.swift
@@ -0,0 +1,17 @@
+//
+//  CheckboxApp.swift
+//  Checkbox
+//
+//  Created by MD  on 4/1/24.
+//
+
+import SwiftUI
+
+@main
+struct CheckboxApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
