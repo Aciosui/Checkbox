d8af75004a1d31968cd1a2f431d90b53bdae55d9
Aciosui <162045930+Aciosui@users.noreply.github.com>
2024-04-02 06:39:20 -0400

    Update CheckboxApp.swift

--- Checkbox/CheckboxApp.swift
+++ Checkbox/CheckboxApp.swift
@@ -11,7 +11,7 @@ import SwiftUI
 struct ContentView: View {
     @State private var isSelected: Bool = false
     var body: some View {
-        Text("Accessible Checbox Example!")
+        Text("Accessible Checkbox Example!")
             .accessibilityAddTraits(.isHeader)
             .font(.title)
                 VStack {
