f4117c12a88a18308292765dc35aca334147cd9e
aciosui <mrana.swe@gmail.com>
2024-04-02 06:36:49 -0400

    update text

--- Checkbox/ContentView.swift
+++ Checkbox/ContentView.swift
@@ -8,14 +8,37 @@
 import SwiftUI
 
 struct ContentView: View {
+    @State private var isSelected: Bool = false
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
+        Text("Accessible Checkbox Example!")
+            .accessibilityAddTraits(.isHeader)
+            .font(.title)
+                VStack {
+            
+            Toggle(isOn: $isSelected){
+                Text("I agree the terms and condition")
+                    .padding()
+            }
+            .toggleStyle(CheckboxToggleStyle())
+            .padding()
         }
-        .padding()
+        .accessibilityElement(children:.contain)
+
+    }
+}
+
+struct CheckboxToggleStyle: ToggleStyle{
+    func makeBody(configuration: Self.Configuration) -> some View {
+        HStack{
+            configuration.label
+            Spacer()
+            Image(systemName: configuration.isOn ? "checkmark.square" : "square")
+                .resizable()
+                .frame(width: 24, height: 24)
+                .onTapGesture {
+                    configuration.isOn.toggle()
+                }
+        }
     }
 }
 
